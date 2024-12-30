71f4a499ab2bf5198cffc2001b5e9b167dfeacd6
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-30 12:04:56 +0100

    Add disabled prop to IconButton

--- src/Ui/common/IconButton/IconButton.jsx
+++ src/Ui/common/IconButton/IconButton.jsx
@@ -3,11 +3,12 @@ import PropTypes from "prop-types";
 import style from "./IconButton.component.scss";
 import { Link } from "react-router";
 
-function IconButton({ onClick, children, active = false, ...props }) {
+function IconButton({ onClick, children, active = false, disabled = false, ...props }) {
 	return (
 		<button
 			onClick={onClick}
-			className={`icon-button ${active && "active"} ${props.className}`}
+			className={`icon-button ${active ? "active" : ""} ${disabled ? "disabled" : ""} ${props.className}`}
+			disabled={disabled}
 			data-style={style}
 			{...props}
 		>
@@ -20,12 +21,18 @@ IconButton.propTypes = {
 	children: PropTypes.node.isRequired,
 	onClick: PropTypes.func,
 	active: PropTypes.bool,
+	disabled: PropTypes.bool,
 	className: PropTypes.string
 };
 
-function IconLinkButton({ to, children, active = false, ...props }) {
+function IconLinkButton({ to, children, active = false, disabled = false, ...props }) {
 	return (
-		<Link to={to} className={`icon-button ${active && "active"} ${props.className}`} data-style={style} {...props}>
+		<Link
+			to={to}
+			className={`icon-button ${active ? "active" : ""} ${disabled ? "disabled" : ""} ${props.className}`}
+			data-style={style}
+			{...props}
+		>
 			{children}
 		</Link>
 	);
@@ -35,6 +42,7 @@ IconLinkButton.propTypes = {
 	children: PropTypes.node.isRequired,
 	to: PropTypes.string.isRequired,
 	active: PropTypes.bool,
+	disabled: PropTypes.bool,
 	className: PropTypes.string
 };
 
