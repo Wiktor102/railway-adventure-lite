bfe5b5371469cab903a20a013f8f39b5798ce1c8
Wiktor Golicz <wiktorgolicz1@gmail.com>
2024-12-31 19:16:33 +0100

    Label when no routes in <RoutesMenu/>

--- src/Ui/pages/RoutesMenu/RoutesMenu.jsx
+++ src/Ui/pages/RoutesMenu/RoutesMenu.jsx
@@ -16,28 +16,31 @@ const RoutesMenu = observer(() => {
 	return (
 		<div className="routes-menu" data-style={style}>
 			<h2>Trasy</h2>
-			<ul className="routes-list">
-				{routeStore.routes.map(route => (
-					<li key={route.id}>
-						<p className="name" style={{ color: route.color }}>
-							{route.name}
-						</p>
-						<p className="route">
-							{route.stations[0]} - {route.stations.at(-1)}
-						</p>
-						<div className="stations">{route.stations.length} przystanków</div>
-						<div className="buttons">
-							<IconLinkButton to={"/game/routes/details/" + route.id}>
-								<i className="fas fa-pencil"></i>
-							</IconLinkButton>
-							<IconButton onClick={() => {}}>
-								<i className="fas fa-trash"></i>
-							</IconButton>
-						</div>
-					</li>
-				))}
-			</ul>
 			<div>
+				{routeStore.routes.length > 0 && (
+					<ul className="routes-list">
+						{routeStore.routes.map(route => (
+							<li key={route.id}>
+								<p className="name" style={{ color: route.color }}>
+									{route.name}
+								</p>
+								<p className="route">
+									{route.stations[0]} - {route.stations.at(-1)}
+								</p>
+								<div className="stations">{route.stations.length} przystanków</div>
+								<div className="buttons">
+									<IconLinkButton to={"/game/routes/details/" + route.id}>
+										<i className="fas fa-pencil"></i>
+									</IconLinkButton>
+									<IconButton onClick={() => {}}>
+										<i className="fas fa-trash"></i>
+									</IconButton>
+								</div>
+							</li>
+						))}
+					</ul>
+				)}
+				{routeStore.routes.length === 0 && <div className="no-routes">Brak tras</div>}
 				<ElevatedLinkButton to="create">
 					<i className="fas fa-plus"></i> Stwórz trasę
 				</ElevatedLinkButton>
