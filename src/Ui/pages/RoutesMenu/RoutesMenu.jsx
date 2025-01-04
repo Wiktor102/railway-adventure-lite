38ed55401b0e957a4c6af5d3c051652eee301dd9
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-01-04 13:37:25 +0100

    Separate list items from <RoutesMenu/> into <RouteTile/>

--- src/Ui/pages/RoutesMenu/RoutesMenu.jsx
+++ src/Ui/pages/RoutesMenu/RoutesMenu.jsx
@@ -9,6 +9,7 @@ import IconButton, { IconLinkButton } from "../../common/IconButton/IconButton";
 
 // style
 import style from "./RoutesMenu.component.scss";
+import tileStyle from "./RouteTile.component.scss";
 
 const RoutesMenu = observer(() => {
 	const { routeStore } = useGameStore();
@@ -20,23 +21,14 @@ const RoutesMenu = observer(() => {
 				{routeStore.routes.length > 0 && (
 					<ul className="routes-list">
 						{routeStore.routes.map(route => (
-							<li key={route.id}>
-								<p className="name" style={{ color: route.color }}>
-									{route.name}
-								</p>
-								<p className="route">
-									{route.stations[0]} - {route.stations.at(-1)}
-								</p>
-								<div className="stations">{route.stations.length} przystanków</div>
-								<div className="buttons">
-									<IconLinkButton to={"/game/routes/details/" + route.id}>
-										<i className="fas fa-pencil"></i>
-									</IconLinkButton>
-									<IconButton onClick={() => {}}>
-										<i className="fas fa-trash"></i>
-									</IconButton>
-								</div>
-							</li>
+							<RouteTile route={route} key={route.id}>
+								<IconLinkButton to={"/game/routes/details/" + route.id} inverted>
+									<i className="fas fa-pencil"></i>
+								</IconLinkButton>
+								<IconButton onClick={() => {}} inverted>
+									<i className="fas fa-trash"></i>
+								</IconButton>
+							</RouteTile>
 						))}
 					</ul>
 				)}
@@ -49,4 +41,20 @@ const RoutesMenu = observer(() => {
 	);
 });
 
+const RouteTile = observer(({ route, children }) => {
+	return (
+		<li data-style={tileStyle} className="route-tile">
+			<p className="name" style={{ color: route.color }}>
+				{route.name}
+			</p>
+			<p className="route">
+				{route.stations[0]} - {route.stations.at(-1)}
+			</p>
+			<div className="stations">{route.stations.length} przystanków</div>
+			<div className="buttons">{children}</div>
+		</li>
+	);
+});
+
+export { RouteTile };
 export default RoutesMenu;
