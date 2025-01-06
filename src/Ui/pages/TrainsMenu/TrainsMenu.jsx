d75788cb7a286061933e9497b887fe387ba93206
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-01-06 21:28:19 +0100

    Refactor TrainsMenu

--- src/Ui/pages/TrainsMenu/TrainsMenu.jsx
+++ src/Ui/pages/TrainsMenu/TrainsMenu.jsx
@@ -28,77 +28,7 @@ const TrainsMenu = observer(() => {
 					<Scrollbars>
 						<ul>
 							{trainStore.trains.map(train => (
-								<li key={train.id}>
-									<img
-										// src="https://static.vecteezy.com/system/resources/previews/019/002/964/non_2x/electric-train-flat-icons-png.png"
-										src={train.type === "unit" ? emuIcon : locomotiveIcon}
-										alt=""
-									/>
-									{!train.route && (
-										<IconLinkButton
-											className="add-route-btn"
-											to={`/game/trains/${train.id}/assign-route`}
-											inverted
-										>
-											<img src={routeAddIcon} alt="Trasa z plusikiem" />
-										</IconLinkButton>
-									)}
-									{train.route && (
-										<IconButton
-											onClick={() => train.assignRoute(null)}
-											className="remove-route-btn"
-											inverted
-										>
-											<img src={routeRemoveIcon} alt="Przekreślona trasa" />
-										</IconButton>
-									)}
-									{train.type === "carriage" && (
-										<IconLinkButton
-											className="carriage-config-btn"
-											to={`/game/trains/${train.id}`}
-											inverted
-										>
-											<i className="fas fa-cog"></i>
-										</IconLinkButton>
-									)}
-									<IconButton inverted>
-										<i className="fas fa-hand-holding-dollar"></i>
-									</IconButton>
-									<div className="data">
-										<p className="speed">
-											<span>
-												<i className="fas fa-gauge"></i> Prędkość maksymalna{" "}
-											</span>
-											<b>{train.speed} km/h</b>
-										</p>
-										{train.type === "carriage" && (
-											<p className="seats">
-												<span>
-													<i className="fas fa-weight-hanging"></i> Liczba wagonów{" "}
-												</span>
-												<b>
-													{train.carriages.length} / {train.maxCarriages}
-												</b>
-											</p>
-										)}
-										<p className="seats">
-											<span>
-												<i className="fas fa-chair"></i> Miejsca siedzące{" "}
-											</span>
-											<b>{train.seats}</b>
-										</p>
-										<p className="route">
-											<span>
-												<i className="fas fa-route"></i> Trasa{" "}
-											</span>
-											<b>
-												{train.route
-													? train.route.stations[0] + " - " + train.route.stations.at(-1)
-													: "Brak"}
-											</b>
-										</p>
-									</div>
-								</li>
+								<TrainTile key={train.id} train={train} />
 							))}
 						</ul>
 					</Scrollbars>
@@ -113,6 +43,74 @@ const TrainsMenu = observer(() => {
 				</ElevatedLinkButton>
 			</div>
 		</>
+	);
+});
+
+const TrainTile = observer(({ train }) => {
+	return (
+		<li>
+			<img
+				// src="https://static.vecteezy.com/system/resources/previews/019/002/964/non_2x/electric-train-flat-icons-png.png"
+				src={train.type === "unit" ? emuIcon : locomotiveIcon}
+				alt=""
+			/>
+			{!train.route && (
+				<IconLinkButton className="add-route-btn" to={`/game/trains/${train.id}/assign-route`} inverted>
+					<img src={routeAddIcon} alt="Trasa z plusikiem" />
+				</IconLinkButton>
+			)}
+			{train.route && (
+				<IconButton onClick={() => train.assignRoute(null)} className="remove-route-btn" inverted>
+					<img src={routeRemoveIcon} alt="Przekreślona trasa" />
+				</IconButton>
+			)}
+			{train.type === "carriage" && (
+				<IconLinkButton className="carriage-config-btn" to={`/game/trains/${train.id}`} inverted>
+					<i className="fas fa-cog"></i>
+				</IconLinkButton>
+			)}
+			<IconButton inverted>
+				<i className="fas fa-hand-holding-dollar"></i>
+			</IconButton>
+			<div className="data">
+				<p className="speed">
+					<span>
+						<i className="fas fa-gauge"></i> Prędkość maksymalna{" "}
+					</span>
+					<b>{train.speed} km/h</b>
+				</p>
+				{train.type === "carriage" && (
+					<p className="seats">
+						<span>
+							<i className="fas fa-weight-hanging"></i> Liczba wagonów{" "}
+						</span>
+						<b>
+							{train.carriages.length} / {train.maxCarriages}
+						</b>
+					</p>
+				)}
+				<p className="seats">
+					<span>
+						<i className="fas fa-chair"></i> Miejsca siedzące{" "}
+					</span>
+					<b>{train.seats}</b>
+				</p>
+				{train.route && (
+					<p className="passengers">
+						<span>
+							<i className="fas fa-users"></i> Pasażerowie{" "}
+						</span>
+						<b>{train.passengers.length}</b>
+					</p>
+				)}
+				<p className="route">
+					<span>
+						<i className="fas fa-route"></i> Trasa{" "}
+					</span>
+					<b>{train.route ? train.route.stations[0] + " - " + train.route.stations.at(-1) : "Brak"}</b>
+				</p>
+			</div>
+		</li>
 	);
 });
 
