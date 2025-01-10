0d15d882215733a3435eee05444677bd41764a08
Wiktor Golicz <wiktorgolicz1@gmail.com>
2025-01-10 21:16:34 +0100

    Save game dialog

--- src/Ui/Ui.jsx
+++ src/Ui/Ui.jsx
@@ -1,6 +1,7 @@
-import { Link, useLocation } from "react-router";
+import { Link, useLocation, useNavigate } from "react-router";
 import { observer } from "mobx-react-lite";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
+import PropTypes from "prop-types";
 
 // hooks
 import { useGameStore } from "../store/GameStoreProvider";
@@ -22,18 +23,70 @@ import moneyImg from "../assets/icons/money.png";
 import style from "./Ui.component.scss";
 import "../Ui/common/loaders.min.css";
 
+const QuitDialog = ({ dialogRef, onClose }) => {
+	const navigate = useNavigate();
+	const gameStore = useGameStore();
+
+	const handleSaveAndQuit = () => {
+		gameStore.saveToLocalStorage();
+		navigate("/");
+	};
+
+	const handleSaveToFile = () => {
+		gameStore.downloadSave();
+	};
+
+	const handleQuitWithoutSave = () => {
+		navigate("/");
+	};
+
+	return (
+		<dialog ref={dialogRef} className="quit-dialog">
+			<h2>Wyjść z gry?</h2>
+			<div className="dialog-buttons">
+				<button className="save-quit-btn green" onClick={handleSaveAndQuit}>
+					Zapisz i wyjdź
+				</button>
+				<button className="green" onClick={handleSaveToFile}>
+					<i className="fas fa-download"></i>
+				</button>
+				<button className="red" onClick={handleQuitWithoutSave}>
+					Wyjdź bez zapisywania
+				</button>
+				<button className="orange" onClick={onClose}>
+					Anuluj
+				</button>
+			</div>
+		</dialog>
+	);
+};
+
+QuitDialog.propTypes = {
+	dialogRef: PropTypes.object,
+	onClose: PropTypes.func
+};
+
 const Ui = observer(() => {
 	const { error, money } = useGameStore();
+	const quitDialogRef = useRef(null);
 	let { pathname } = useLocation();
 	pathname = pathname.substring(6);
 	const hasTip = useIsNamedRouteRendering("tips");
+
+	const handleQuit = () => {
+		quitDialogRef.current?.showModal();
+	};
+
+	const handleCloseDialog = () => {
+		quitDialogRef.current?.close();
+	};
 
 	return (
 		<div className="game-ui" data-style={style}>
 			<div className="game-ui-top">
 				<SpeedChanger />
 				<StatusBox icon={moneyImg} value={money.toLocaleString("en-US").replace(/,/g, " ")} />
-				<IconButton onClick={() => {}}>
+				<IconButton onClick={handleQuit}>
 					<i className="fa-solid fa-arrow-right-from-bracket"></i>
 				</IconButton>
 			</div>
@@ -81,6 +134,7 @@ const Ui = observer(() => {
 			<div className="game-ui-bottom-center">
 				<div className="error-banner">{error}</div>
 			</div>
+			<QuitDialog dialogRef={quitDialogRef} onClose={handleCloseDialog} />
 		</div>
 	);
 });
